e6f5cbd87d95261cbf20ab1bd9d83c1af9126632
youngtrips <youngtrips@gmail.com>
2013-05-29 20:44:56 +0800

    update

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/logit/"
+http_images_path = "/logit/images"
+http_fonts_path = "/logit/fonts"
+css_dir = "public/logit/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
